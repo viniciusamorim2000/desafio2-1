6a256287f37f2c515cb0d17ec6e2301802abfdf0
Vinícius Amorim da Silva <amorimhavoc@gmail.com>
2022-03-16 22:26:11 -0300

    Iniciando projeto

---
+++ src/main/java/com/example/dock/DockApplication.java
@@ -0,0 +1,13 @@
+package com.example.dock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockApplication.class, args);
+    }
+
+}
